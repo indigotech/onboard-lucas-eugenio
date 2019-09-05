02b5570c6e9f2d2627e9daec535bb7c3edd69086
Taqtile <taqtile@Way.local>
2019-09-05 18:23:15 -0300

    Add first User List

--- MyAwesomeProject/index.js
+++ MyAwesomeProject/index.js
@@ -6,5 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { Login } from './components/Login';
+import { UserList } from './components/UserList'
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => UserList);
